f7c69bda9c80d7d8e8b89ef24d1a3b302388412b
Laurence Alldis <laurence.alldis@bdo.co.uk>
2024-08-28 15:45:46 +0100

    Init repository

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using Azure.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Configuration
+   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+   .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+   .AddEnvironmentVariables();
+
+builder.Configuration.AddAzureKeyVault(
+    new Uri(builder.Configuration["KeyVault:VaultUri"]!),
+    new AzureCliCredential() // You can change this to DefaultAzureCredential() if it's easier
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Local"))
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/specialmessage", () =>
+{
+    var secret = app.Configuration["MessageCentre:Message"];
+    var response = $"Don't actually print secrets like \"{secret}\" please";
+    return response;
+})
+.WithName("GetSpecialMessage")
+.WithOpenApi();
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,6 @@
+# README
+
+## Setup steps
+- Copy `appsettings.Local.json.template` to `appsettings.Local.json`
+- Update the `VaultUri` property to your Key Vault's Uri
+- Add a secret to your Key Vault called `MessageCentre--Message`
